9a060ec775772f7da3cee6389bf621dde24bf6b6
ResidualResidue <35879370+ResidualResidue@users.noreply.github.com>
2019-10-08 23:24:39 -0400

    Update comments on Quicksort.java

--- QuickSort.java
+++ QuickSort.java
@@ -9,20 +9,29 @@ public class QuickSort {
 	}
 	private static void quickSort(int nums[], int low, int high) {
 		if(low < high) {
-		
+			//Returns the partition index of where everything to the left is less than
+			//The value at the index of the pi, and everything to the right of it is greater.
 			int pi = partition(nums, low, high);
-		
+			//Exclusively left and exlcusively right is recursively quicksorted.
 			quickSort(nums, low, pi-1);
 			quickSort(nums, pi+1, high);
 		}
 		
 	}
 	private static int partition(int nums[], int low, int high) {
+		//Pivot point is the last index.
 		int pivot = high;
 		
-		//i sorts the 
+		//Goes through all values up to but not including pivot. 
+		//i keeps track of the index where pivot is gonna go.
 		int i = low;
 		for(int j = low; j<high; j++) {
+			/*
+			 * j iterates through whole array. 
+			 * j being less than or equal to the pivot switches it with the current value of i
+			 * and increments i.
+			 * i indicates the place where the pivot itself will eventually go.
+			 */
 			if(nums[j] <= nums[pivot]) {
 				int temp = nums[j];
 				nums[j] = nums[i];
@@ -31,6 +40,7 @@ public class QuickSort {
 				
 			}
 		}
+		//Switches pivot with value at i.
 		int temp = nums[high];
 		nums[high] = nums[i];
 		nums[i]= temp;
